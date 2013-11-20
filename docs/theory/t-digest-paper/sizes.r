c2712279ad4b09f14d3998653f4f55f4689ef719
Ted Dunning <tdunning@apache.org>
2013-11-19 23:43:10 -0800

    Fixed terminology, redrew figures with current size info

--- docs/theory/t-digest-paper/sizes.r
+++ docs/theory/t-digest-paper/sizes.r
@@ -1,22 +1,32 @@
-png("k-approx.png", width=1800, height=1800, pointsize=72)
 data = read.delim("sizes.csv")
 
-plotGraph = function(tag, title='') {
+plotGraph = function(tag, title='', showY=T) {
   n = max(data[data$tag == tag, ]$i)
   i = 1:n
   n2 = n/2
 
-  png(paste(tag, "-sizes.png", sep=''), width=1800, height=1800, pointsize=72)
-  plot(actual~q, data[data$tag == tag,], cex=0.2, xaxt='n', xlim=c(0,1), ylim=c(0,1050), xlab='Cluster', ylab='Centroid size')
+  if (showY) {
+    yaxt = 's'
+  } else {
+    yaxt = 'n'
+  }
+
+  plot(actual~q, data[data$tag == tag,], cex=0.2, xaxt='n', xlim=c(0,1), ylim=c(0,1050), xlab='Quantile',
+       ylab='Centroid size', yaxt=yaxt)
   title(title)
   box(lwd=3)
   axis(side=1, at=c(0,0.25, 0.5, 0.75, 1), labels=c(0.0,0.25,0.5,0.75, 1.0), lwd=3)
 
   q = seq(0,1,by=0.01)
-  lines(q, 1000*4*q*(1-q), lwd=9)
-  dev.off()
+  lines(q, 1000*4*q*(1-q), lwd=3, col='gray')
 }
 
-plotGraph("gamma", title="Gamma(0.1, 0.1) Distribution")
-plotGraph("uniform", title="Uniform Distribution")
-plotGraph("mixture", title="Mixture Distribution")
+png("sizes.png", width=1800, height=700, pointsize=36)
+layout(matrix(c(1,2,3), 1, 3, byrow=T), widths=c(1.21,1,1))
+plotGraph("uniform", title="Uniform Distribution", T)
+old = par(mar=c(5.1,0,4.1,2))
+plotGraph("gamma", title="Gamma(0.1, 0.1) Distribution", F)
+plotGraph("sequential", title="Sequential Distribution", F)
+par(old)
+  dev.off()
+#plotGraph("mixture", title="Mixture Distribution")
