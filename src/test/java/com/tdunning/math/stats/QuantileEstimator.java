386b8151fed302d60808a4d5974dd5df86496081
Ted Dunning <tdunning@apache.org>
2014-03-15 22:13:38 -0700

    Updated copyright

--- src/test/java/com/tdunning/math/stats/QuantileEstimator.java
+++ src/test/java/com/tdunning/math/stats/QuantileEstimator.java
@@ -1,3 +1,24 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Copied verbatim from Datafu which originally had the implementation from
+ * Google's Sawzall.  This version is only used for testing.
+ */
 package com.tdunning.math.stats;
 
 import java.util.ArrayList;
@@ -6,8 +27,6 @@ import java.util.List;
 
 /**
  * Quantile estimation removed from DataFu library class StreamingQuantile for comparison.
- * <p/>
- * This is
  */
 public class QuantileEstimator {
     private static final long MAX_TOT_ELEMS = 1024L * 1024L * 1024L * 1024L;
