1208ae0c10b8b25c19df61a068d32512458e9e3a
Ted Dunning <ted.dunning@gmail.com>
2018-10-31 20:42:00 -0700

    Added scale function tests, small fixes. Also updated sizing.tex document with fixes.

---
+++ core/src/main/java/com/tdunning/math/stats/ScaleFunction.java
@@ -0,0 +1,7 @@
+package com.tdunning.math.stats;
+
+/**
+ * Created by tdunning on 10/30/18.
+ */
+public class ScaleFunction {
+}
---
+++ core/src/test/java/com/tdunning/math/stats/ScaleFunctionTests.java
@@ -0,0 +1,7 @@
+package com.tdunning.scale;
+
+/**
+ * Created by tdunning on 10/31/18.
+ */
+public class ScaleFunctionTests {
+}
